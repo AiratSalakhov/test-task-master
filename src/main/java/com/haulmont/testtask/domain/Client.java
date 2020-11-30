e87fb96ded501d123e503702ebe59cd5bcd90bf3
AiratSalakhov <tlt_asm1@mail.ru>
2020-12-01 02:11:17 +0400

    1201_1

---
+++ src/main/java/com/haulmont/testtask/domain/Client.java
@@ -0,0 +1,56 @@
+package com.haulmont.testtask.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CLIENTS")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+    @Column(name = "middlename")
+    private String middleName;
+    @Column(name = "phonenumber")
+    private String phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
